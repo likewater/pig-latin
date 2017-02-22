edcb26f3dc8034d9c50829ae008a7c97e2dbad73
Blake Womack and Ryan McAlpin <ryan@>
2017-02-22 15:02:48 -0800

    transfrom multiple words into array of strings and translate each

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
 $(function() {
+  var resultArray = [];
   var pigLatin = function(userInput) {
-
-    // debugger;
-    if (consonant(userInput.charAt(0))){
-      //consonants
-      for (var i = 0; i < userInput.length; i++) {
-        //for every character
-        var firstLetter = userInput.slice(0, 1);
-        //if first is still consonant
-        if (consonant(firstLetter)) {
-          userInput = userInput.slice(1, (userInput.length));
-          userInput = userInput.concat(firstLetter);
+    var words = userInput.split(" ");
+    words.forEach(function(currentWord) {
+      console.log(currentWord);
+      // debugger;
+      if (consonant(currentWord.charAt(0))){
+        //consonants
+        for (var i = 0; i < currentWord.length; i++) {
+          //for every character
+          var firstLetter = currentWord.slice(0, 1);
+          //if first is still consonant
+          if (consonant(firstLetter)) {
+            currentWord = currentWord.slice(1, (currentWord.length));
+            currentWord = currentWord.concat(firstLetter);
+          } else {
+            //do nothing
+          }
         }
+        var result = currentWord.concat("ay");
+      } else {
+        var result = currentWord.concat("ay");
       }
-      var result = userInput.concat("ay")
-      return result;
-      alert(result);
-    } else {
-      result = userInput.concat("ay");
-      return result;
-    }
+      resultArray.push(result);
+    });
+
+    //join the arrray!!
+
+
+    console.log(resultArray);
+    // console.log(words);
+
   }
   var consonant = function(letter) {
     if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
@@ -28,27 +39,10 @@ $(function() {
       return true;
     }
   }
-  // var consonant = function(letter){
-  //   if ((letter === "a")){
-  //     return false;
-  //   } else if (letter === "e") {
-  //     return false;
-  //   } else if (letter === "i") {
-  //     return false;
-  //   } else if (letter === "o"){
-  //     return false;
-  //   } else if (letter === "u") {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
-
 
   $("form").submit(function(){
     event.preventDefault();
     var userInput = $("#text-input").val();
     var result = pigLatin(userInput);
-    alert(result);
   });
 });
